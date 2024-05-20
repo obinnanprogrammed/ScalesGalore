f6904cb586bdccfc91a547cc150f9e864b3effe6
Obinna Nwakwue <obinnanwakwue10@gmail.com>
2024-05-20 17:14:34 -0500

    attempting to render vexflow

--- ScaleDictionary/App.js
+++ ScaleDictionary/App.js
@@ -3,24 +3,32 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import * as Screen from './screens';
+import Welcome from './screens/Welcome.js';
+import Interface from './screens/Interface.js';
+import { registerRootComponent } from 'expo';
 
 const Stack = createNativeStackNavigator();
 
-export default function App() {
-  <NavigationContainer>
-    <Stack.Navigator initialRouteName="Welcome">
-      <Stack.Screen options={{ headerShown: false }} name="Welcome" component={Screen.Welcome}></Stack.Screen>
-      <Stack.Screen options={{ headerShown: false }} name="Interface" component={Screen.Interface}></Stack.Screen>
-    </Stack.Navigator>
-  </NavigationContainer>
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Welcome">
+        <Stack.Screen options={{ headerShown: false }} name="Welcome" component={Welcome}></Stack.Screen>
+        <Stack.Screen options={{ headerShown: false }} name="Interface" component={Interface}></Stack.Screen>
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
+    color: 'black',
     alignItems: 'center',
     justifyContent: 'center',
   },
 });
+
+registerRootComponent(App);
+export default App;
--- ScaleDictionary/screens/Welcome.js
+++ ScaleDictionary/screens/Welcome.js
@@ -1,13 +1,23 @@
 import * as React from 'react';
-import { useRoute } from '@react-navigation/native';
-import { View, Text, Pressable, StyleSheet } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import { View, Text, Pressable, StyleSheet, Button } from 'react-native';
 
 function Welcome() {
+    const navigation = useNavigation();
     return (
-        <View>
-            <Text>Welcome.</Text>
+        <View style={styles.container}>
+            <Text>Welcome. Click to get started</Text>
+            <Button title="To Interface" onPress={() => { navigation.navigate("Interface")}} />
         </View>
     )
 }
 
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    }
+});
 export default Welcome;
